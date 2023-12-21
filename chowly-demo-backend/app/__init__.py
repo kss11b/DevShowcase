db0ece4fa4bf93c62bb6e8274e7bcb9219ccb4dd
kss11b <kss11b@users.noreply.github.com>
2023-12-21 17:28:50 -0600

    main: Added changes to ready project for deployment

--- chowly-demo-backend/app/__init__.py
+++ chowly-demo-backend/app/__init__.py
@@ -1,10 +1,14 @@
 import logging
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://test:password@db:5432/chowly-demo-db'
+app.config['POSTGRES_DB'] = os.environ.get('POSTGRES_DB', 'chowly-demo-db')
+app.config['POSTGRES_USER'] = os.environ.get('POSTGRES_USER', 'test')
+app.config['POSTGRES_PASSWORD'] = os.environ.get('POSTGRES_PASSWORD', 'password')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{app.config["POSTGRES_USER"]}:{app.config["POSTGRES_PASSWORD"]}@db:5432/{app.config["POSTGRES_DB"]}'
 db = SQLAlchemy(app)
 
 from . import routes, models
