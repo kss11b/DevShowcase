aa94b68040b4e7f2e33e82dac4d8342e14c31bf9
kss11b <kss11b@users.noreply.github.com>
2023-12-22 13:43:06 -0600

    main: Updated workflow and updated usage of env on flask app

--- chowly-demo-backend/app/__init__.py
+++ chowly-demo-backend/app/__init__.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 app.config['POSTGRES_DB'] = os.environ.get('POSTGRES_DB', 'chowly-demo-db')
 app.config['POSTGRES_USER'] = os.environ.get('POSTGRES_USER', 'test')
 app.config['POSTGRES_PASSWORD'] = os.environ.get('POSTGRES_PASSWORD', 'password')
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{app.config["POSTGRES_USER"]}:{app.config["POSTGRES_PASSWORD"]}@db:5432/{app.config["POSTGRES_DB"]}'
+app.config['POSTGRES_URL'] = os.environ.get('POSTGRES_URL', 'db:5432')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{app.config["POSTGRES_USER"]}:{app.config["POSTGRES_PASSWORD"]}@{app.config["POSTGRES_URL"]}/{app.config["POSTGRES_DB"]}'
 db = SQLAlchemy(app)
 
 from . import routes, models
